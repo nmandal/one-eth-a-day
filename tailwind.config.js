5762f4225b484e9ccb149d9b1ecae9b560620fca
nmandal <nicholas.mandal@gmail.com>
2021-11-30 11:17:48 -0600

    colors not showing up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
+  purge: [],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        sky: colors.sky,
       },
     },
   },
